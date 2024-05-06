9aeb54d038a975aa639fbb88da53a78f376147b7
zidanindratama <zidanindratama31@gmail.com>
2024-05-07 04:21:47 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
 
   app.enableCors({
     allowedHeaders: ['content-type'],
-    origin: '*',
+    origin: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
     credentials: true,
   });
