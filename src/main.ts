c0522d1fcaecb1630b04aa5f8a097c4fe614f4f3
zidanindratama <zidanindratama31@gmail.com>
2024-05-07 03:32:09 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,20 @@ import { Request, Response, NextFunction } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const whitelist = [
+    'http://localhost:3000',
+    'https://auction-app-pbo.vercel.app',
+  ];
   app.enableCors({
-    // origin: ['http://localhost:3000', 'https://auction-app-pbo.vercel.app'],
-    origin: '*',
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     allowedHeaders: ['Content-Type', 'Authorization'],
+    preflightContinue: true,
     credentials: true,
   });
   app.use(cookieParser());
