abb5952b645d74305d26f691adf933459051c68a
zidanindratama <zidanindratama31@gmail.com>
2024-05-07 04:00:36 +0700

    cors ga kelar-kelar ajg

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import * as cookieParser from 'cookie-parser';
 import cookieParser from 'cookie-parser';
 import { Request, Response, NextFunction } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const whitelist = [
-    'http://localhost:3000',
-    'https://auction-app-pbo.vercel.app',
-  ];
+
   app.enableCors({
-    origin: function (origin, callback) {
-      if (!origin || whitelist.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    preflightContinue: true,
+    allowedHeaders: ['content-type'],
+    origin: [
+      'http://localhost:3000',
+      'https://auction-app-pbo.vercel.app/sign-in',
+    ],
     credentials: true,
   });
   app.use(cookieParser());
