8263768267a1568e03c19824a112ad0a98262991
zidanindratama <zidanindratama31@gmail.com>
2024-05-07 04:25:38 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,26 @@ import { AppModule } from './app.module';
 // import * as cookieParser from 'cookie-parser';
 import cookieParser from 'cookie-parser';
 import { Request, Response, NextFunction } from 'express';
+import cors from 'cors-ts';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    allowedHeaders: ['content-type'],
-    origin: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
-    credentials: true,
-  });
+  app.use(
+    cors({
+      allowedHeaders: ['content-type'],
+      origin: 'https://auction-app-pbo.vercel.app/',
+      optionsSuccessStatus: 200,
+      credentials: true,
+    }),
+  );
+
+  // app.enableCors({
+  //   allowedHeaders: ['content-type'],
+  //   origin: true,
+  //   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+  //   credentials: true,
+  // });
   app.use(cookieParser());
   await app.listen(3100);
 }
