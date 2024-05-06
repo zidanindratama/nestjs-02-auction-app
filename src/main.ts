2280e7f39f99aadccc64649d6f383c5cfdafd9a0
zidanindratama <zidanindratama31@gmail.com>
2024-05-07 05:12:45 +0700

    ya allah

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,40 @@ import cors from 'cors-ts';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const whitelist = [
+    'https://auction-app-pbo.vercel.app',
+    'http://localhost:3000',
+  ];
+
   app.enableCors({
-    origin: true,
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('CORS ERROR'));
+      }
+    },
+    allowedHeaders: [
+      'Access-Control-Request-Method',
+      'Access-Control-Request-Headers',
+      'Access-Control-Allow-Methods',
+      'Access-Control-Allow-Credentials',
+      'Access-Control-Allow-Headers',
+      'Access-Control-Allow-Origin',
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'Authorization',
+      'X-HTTP-Method-Override',
+      'Set-Cookie',
+      'Cookie',
+      'Request',
+      'withCredentials',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
     credentials: true,
+    optionsSuccessStatus: 200,
   });
 
   app.use(cookieParser());
