6776fe3885ca101f2b6dfc96aad45cb095b7b424
zidanindratama <zidanindratama31@gmail.com>
2024-05-07 05:42:44 +0700

    cors anjinggg

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 // import * as cookieParser from 'cookie-parser';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import { Request, Response, NextFunction } from 'express';
-import cors from 'cors-ts';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,36 +13,73 @@ async function bootstrap() {
     'http://localhost:3000',
   ];
 
-  app.enableCors({
-    origin: function (origin, callback) {
-      if (!origin || whitelist.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('CORS ERROR'));
-      }
-    },
-    allowedHeaders: [
-      'Access-Control-Request-Method',
-      'Access-Control-Request-Headers',
-      'Access-Control-Allow-Methods',
-      'Access-Control-Allow-Credentials',
-      'Access-Control-Allow-Headers',
-      'Access-Control-Allow-Origin',
-      'Origin',
-      'X-Requested-With',
-      'Content-Type',
-      'Accept',
-      'Authorization',
-      'X-HTTP-Method-Override',
-      'Set-Cookie',
-      'Cookie',
-      'Request',
-      'withCredentials',
-    ],
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-    credentials: true,
-    optionsSuccessStatus: 200,
-  });
+  // app.enableCors({
+  //   origin: function (origin, callback) {
+  //     if (!origin || whitelist.indexOf(origin) !== -1) {
+  //       callback(null, true);
+  //     } else {
+  //       callback(new Error('CORS ERROR'));
+  //     }
+  //   },
+  //   allowedHeaders: [
+  //     'Access-Control-Request-Method',
+  //     'Access-Control-Request-Headers',
+  //     'Access-Control-Allow-Methods',
+  //     'Access-Control-Allow-Credentials',
+  //     'Access-Control-Allow-Headers',
+  //     'Access-Control-Allow-Origin',
+  //     'Origin',
+  //     'X-Requested-With',
+  //     'Content-Type',
+  //     'Accept',
+  //     'Authorization',
+  //     'X-HTTP-Method-Override',
+  //     'Set-Cookie',
+  //     'Cookie',
+  //     'Request',
+  //     'withCredentials',
+  //   ],
+  //   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+  //   credentials: true,
+  //   optionsSuccessStatus: 200,
+  //   // @ts-ignore
+  //   origin: ['http://localhost:3000', 'https://tax-center-brevet.vercel.app'],
+  // });
+
+  app.use(
+    cors({
+      origin: function (origin, callback) {
+        if (!origin || whitelist.indexOf(origin) !== -1) {
+          callback(null, true);
+        } else {
+          callback(new Error('CORS ERROR'));
+        }
+      },
+      allowedHeaders: [
+        'Access-Control-Request-Method',
+        'Access-Control-Request-Headers',
+        'Access-Control-Allow-Methods',
+        'Access-Control-Allow-Credentials',
+        'Access-Control-Allow-Headers',
+        'Access-Control-Allow-Origin',
+        'Origin',
+        'X-Requested-With',
+        'Content-Type',
+        'Accept',
+        'Authorization',
+        'X-HTTP-Method-Override',
+        'Set-Cookie',
+        'Cookie',
+        'Request',
+        'withCredentials',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      credentials: true,
+      optionsSuccessStatus: 200,
+      // @ts-ignore
+      origin: ['http://localhost:3000', 'https://tax-center-brevet.vercel.app'],
+    }),
+  );
 
   app.use(cookieParser());
   await app.listen(3100);
